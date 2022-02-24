92341ce260fdc7e40a42108f14af83bf2c155e67
Kevin Do <kevind0501@gmail.com>
2022-02-24 15:17:23 -0800

    redirect user when logging out

--- src/components/AuthButton/AuthButton.jsx
+++ src/components/AuthButton/AuthButton.jsx
@@ -1,9 +1,11 @@
 import { useUser } from '../../context/UserContext';
 import { Link } from 'react-router-dom';
 import { signOutUser } from '../../services/users';
+import { useHistory } from 'react-router-dom/';
 
 export default function AuthButton() {
   const { user, setUser } = useUser();
+  const history = useHistory();
   const button =
     'inline-block text-sm px-4 py-2 leading-none border rounded text-slate-200 border-slate-200 hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0';
 
@@ -15,12 +17,13 @@ export default function AuthButton() {
           onClick={async () => {
             await signOutUser();
             setUser({});
+            history.push('/');
           }}
         >
           Sign Out
         </button>
       ) : (
-        <Link to="/login" className={className}>
+        <Link to="/login">
           <button className={button}>Sign In</button>
         </Link>
       )}
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -8,6 +8,9 @@ export default function Home() {
         <h1 className="text-slate-200 mt-8 ml-16 text-3xl">
           Welcome to Acme Inc. Employee Directory!
         </h1>
+        <p className="text-slate-200 mt-8 ml-16 text-xl">
+          Please sign in using the button above
+        </p>
       </div>
     </>
   );
