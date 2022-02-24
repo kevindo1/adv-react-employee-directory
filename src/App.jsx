da8f20550fbaeff5e05a1e2553f44ff4adc85e40
Kevin Do <kevind0501@gmail.com>
2022-02-23 16:58:46 -0800

    added in browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Home from './views/Home/Home';
+import Login from './views/Login/Login';
+import Create from './views/Profile/Create/Create';
+import Edit from './views/Profile/Edit/Edit';
+import Profile from './views/Profile/Profile';
+import Register from './views/Register/Register';
 
 export default function App() {
   return (
-    <h1
-      className={`
+    <>
+      <h1
+        className={`
       bg-green-400
         text-3xl
         text-center
@@ -15,8 +23,31 @@ export default function App() {
         mt-10
         myCustomCssClass
       `}
-    >
-      Hello, World!
-    </h1>
+      >
+        Hello, World!
+      </h1>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/register">
+            <Register />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/profile">
+            <Profile />
+          </Route>
+          <Route exact path="/profile/edit">
+            <Edit />
+          </Route>
+          <Route exact path="/profile/create">
+            <Create />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
